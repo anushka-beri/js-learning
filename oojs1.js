fc47a6995c1882692f8845639f24a4b1ecc5b2b3
thisbutterfly <berianushka@gmail.com>
2020-06-03 13:45:48 +0530

    solved oojs1 from mdn

---
+++ oojs1.js
@@ -0,0 +1,14 @@
+function Shape(name, sides, sideLength) {
+  this.name = name;
+  this.sides = sides;
+  this.sideLength = sideLength;
+  this.calcPerimeter = function () {
+    console.log(sideLength * sides);
+  };
+}
+
+// Write your code below here
+let square = new Shape("square", 4, 5);
+square.calcPerimeter();
+let triangle = new Shape("triangle", 3, 3);
+triangle.calcPerimeter();
