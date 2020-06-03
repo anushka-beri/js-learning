8223badb59a3e8d99e05e0b44cada8143604533b
thisbutterfly <berianushka@gmail.com>
2020-06-03 12:58:36 +0530

    bio created using constructor functions

--- constructorFunctions.js
+++ constructorFunctions.js
@@ -1,14 +1,23 @@
-function Person(name){
-    this.name = name;
+function Person(fname, lname, age, gender, interests){
+    this.name = {
+        first : fname,
+        last: lname
+    }
+    this.age = age;
+    this.gender= gender;
+    this.interests = interests;
+    this.bio = function(){
+        console.log(this.name.first + ' ' + this.name.last + " " + 'is' + " " + this.age + " " + 'years old.' + ""+ this.gender + " " +'likes' + " " + this.interests[0] + " " + 'and' +" " +  this.interests[1])
+    }
     this.greetings = function(){
-        console.log("Hi I am " + this.name + ".")
+        console.log("Hi I am" + this.name.first)
     }
 }
 
-let person1 = new Person('Bob')
-let person2 = new Person('Sarah')
+let person1 = new Person('Bob') // new tells the browser to create a new object
+let person2 = new Person('Bob', 'Smith', 32, 'He', ['music', 'skiing']);
 
-person1.name
-person1.greetings()
+// person1.name
+// person1.greetings()
 person2.name
-person2.greetings()
+person2.bio()
