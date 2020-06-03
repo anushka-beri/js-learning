00d453573cf62b110e1cb1c4be0b81d237a074e5
thisbutterfly <berianushka@gmail.com>
2020-06-03 12:02:34 +0530

    constructor function

---
+++ objects.js
@@ -0,0 +1,13 @@
+function createNewPerson(name){
+    const obj = {};
+    obj.name = name;
+    obj.greeting = function(){
+        console.log("Hi I am " + obj.name + ".")
+    }
+    return obj;
+}
+
+const salva = createNewPerson('Salva')
+salva.name;
+salva.greeting();
+
